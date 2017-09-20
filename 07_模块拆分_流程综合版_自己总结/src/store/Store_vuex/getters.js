49125fa2e44632501fd655431d7642ca15b5bcbd
Doing <helong@yuelu.net>
2017-09-20 15:38:42 +0800

    20170920

---
+++ 07_模块拆分_流程综合版_自己总结/src/store/Store_vuex/getters.js
@@ -0,0 +1,14 @@
+// 此处和state相对应，用来传递state中的数据
+// 可以看做是计算属性做二次处理
+export default {
+  // 积分
+  score (state) {
+    return state.score
+  },
+  // 价格
+  price (state) {
+    if (state.score) {
+      return state.price + 9
+    }
+  }
+}
